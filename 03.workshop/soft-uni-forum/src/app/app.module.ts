59727a62342e95f559e213f81b2926b69b47f888
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2022-03-13 18:02:46 +0000

    workshop

---
+++ 03.workshop/soft-uni-forum/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { UserService } from './user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent,
+    ThemeListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    UserService
+  ],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class AppModule { }
